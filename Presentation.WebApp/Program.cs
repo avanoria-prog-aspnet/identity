1adae5679700fd04d4f7501a5aec1fd7bda5d7ba
Hans Mattin-Lassei <hans.mattin-lassei@avanoria.se>
2026-03-24 08:54:02 +0100

    Add project files.

---
+++ Presentation.WebApp/Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRouting(x => x.LowercaseUrls = true);
+
+var app = builder.Build();
+
+app.UseHsts();
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
